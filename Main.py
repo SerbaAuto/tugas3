e5a788177b36b1df5a1504d8ce604911185cbba4
SerbaAuto <87936278+SerbaAuto@users.noreply.github.com>
2023-03-18 17:24:05 +0700

    -

--- Main.py
+++ Main.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import altair as alt
+import plotly.figure_factory as ff
+import spicy as s
 
 
 # 1.  Write and Magic
@@ -124,6 +126,42 @@ c = alt.Chart(chart_data).mark_circle().encode(
     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
 
 st.altair_chart(c, use_container_width=True)
+
+st.write('''
+# 4f. Streamlit Vega Lite Chart
+''')
+
+st.vega_lite_chart(chart_data, {
+    'mark': {'type': 'circle', 'tooltip': True},
+    'encoding': {
+        'x': {'field': 'a', 'type': 'quantitative'},
+        'y': {'field': 'b', 'type': 'quantitative'},
+        'size': {'field': 'c', 'type': 'quantitative'},
+        'color': {'field': 'c', 'type': 'quantitative'},
+    },
+})
+
+st.write('''
+# 4f. Streamlit Plotly Chart
+''')
+
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+        hist_data, group_labels, bin_size=[.1, .25, .5])
+
+
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
 
 # 5.  Input Widget
 # 6.  Media Element
