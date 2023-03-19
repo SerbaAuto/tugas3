f1b10e2818e99f565bfa4da6b59a53dd6dfd49f6
SerbaAuto <87936278+SerbaAuto@users.noreply.github.com>
2023-03-19 13:01:58 +0700

    -

--- Main.py
+++ Main.py
@@ -260,11 +260,13 @@ st.header("5b. Streamlit Experimental_data_editor")
 df = pd.DataFrame(
     [
        {"command": "st.selectbox", "rating": 4, "is_widget": True},
-       {"command": "st.balloons", "rating": 5, "is_widget": False},
+       {"command": "st.balloons", "rating": 3, "is_widget": False},
        {"command": "st.time_input", "rating": 3, "is_widget": True},
    ]
 )
+
 edited_df = st.experimental_data_editor(df)
+
 
 favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
 st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
