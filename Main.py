832bfd57b0ea28c3196aedb6746dec7778a11f8e
SerbaAuto <87936278+SerbaAuto@users.noreply.github.com>
2023-03-19 17:06:27 +0700

    -

--- Main.py
+++ Main.py
@@ -10,379 +10,403 @@ import graphviz
 import datetime
 from PIL import Image
 import os
+import time
+
+tab1,tab2,tab3,tab4,tab5,tab6,tab7,tab8,tab9,tab10=st.tabs(["Write & Magic","Text Element","Data Display","Chart","Input","Media","Layout","Status","Control Flow","Utilities"])
 
 
-# 1.  Write and Magic
-st.write('''
-# 1. Write and Magic
+# Using "with" notation
+with st.sidebar:
+    st.title("Daftar Isi | Layout Sidebar & Expander")
+    with st.expander("Nomor 1"):
+        st.markdown("[1. Write and Magic](#1-write-and-magic)")
+    with st.expander("Nomor 2"):
+        st.markdown("[2. Text Elements](#2-text-elements)")
+        st.markdown("[2b. Streamlit Title](#2b-streamlit-title)")
+        st.markdown("[2c. Header](#2c-header)")
+        st.markdown("[2d. Sub Header](#2d-sub-header)")
+        st.markdown("[2e. Caption]")
+        st.markdown("[2f. Code](#2f-code)")
+        st.markdown("[2g. Streamlit Text](#2g-streamlit-text)")
+        st.markdown("[2h. Streamlit Latex](#2h-streamlit-latex)")
+    with st.expander("Nomor 3"):
+        st.markdown("[3. Data Display Element](#3-data-display-element)")
+        st.markdown("[3a. Streamlit Dataframe](#3a-streamlit-dataframe)")
+        st.markdown("[3b. Streamlit Table](#3b-streamlit-table)")
+        st.markdown("[3c. Strealit Metric](#3c-strealit-metric)")
+        st.markdown("[3d. Strealit json](#3d-strealit-json)")
 
-This is a _magic_.
-''')
+
+st.header("4. Chart Element")
+st.header("4a. Strealit Line Chart")
 
 
-# 2.  Text Element
-st.write('''
-# 2. Text Elements
-''')
-         
-st.markdown(" 2a. Streamlit *_Mark_down*")
-st.markdown("Warna :red[Merah] dan Biru bold **:blue[Biru]**.")
+with tab1:
+    st.header("1. Write and Magic")
+    # 1.  Write and Magic
+    st.write('''
+    # This is *_Magic_*
+    ''')
+    
 
-st.title("2b. Streamlit Title")
 
-st.header("2c. Header")
-st.subheader("2d. Sub Header")
-st.caption("2e. Caption")
+with tab2:
+    # 2.  Text Element
+    st.header("2. Text Elements")
+            
+    st.markdown(" 2a. Streamlit *_Mark_down*")
+    st.markdown("Warna :red[Merah] dan Biru bold **:blue[Biru]**.")
 
-#2f. st.code
-code = '''
-    def HelloWorld():
-        print("2f. Streamlit Code")
-        print("Hello World")
-'''
+    st.title("2b. Streamlit Title")
+    st.header("2c. Header")
+    st.subheader("2d. Sub Header")
+    st.caption("2e. Caption")
 
-st.code(code,language="python")
+    st.header("2f. Code")
+    #2f. st.code
+    code = '''
+        def HelloWorld():
+            print("2f. Streamlit Code")
+            print("Hello World")
+    '''
 
-st.text("2g. Streamlit Text")
+    st.code(code,language="python")
 
-st.write('''
-# 2h. Streamlit Latex
-''')
-st.latex(r'''
-    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} = \sum_{k=0}^{n-1} ar^k =  a \left(\frac{1-r^{n}}{1-r}\right)
+    st.text("2g. Streamlit Text")
+
+    st.write('''
+    # 2h. Streamlit Latex
+    ''')
+    st.latex(r'''
+        a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} = \sum_{k=0}^{n-1} ar^k =  a \left(\frac{1-r^{n}}{1-r}\right)
+        ''')
+
+with tab3:
+    # 3.  Data Display Element
+    st.header("3. Data Display Element")
+    st.header("3a. Streamlit Dataframe")
+    df = pd.DataFrame(
+    np.random.randn(50, 3),
+    columns=('col %d' % i for i in range(3)))
+
+    st.dataframe(df)
+    st.header("3b. Streamlit Table")
+    st.table(df)
+
+    st.header("3c. Strealit Metric")
+
+    col1, col2, col3 = st.columns(3)
+    col1.metric("Temperature", "70 °F", "1.2 °F")
+    col2.metric("Wind", "9 mph", "-8%")
+    col3.metric("Humidity", "86%", "4%")
+    col3.metric(label="Humidity",value="86%",delta="4%",delta_color="inverse")
+
+    st.header("3d. Strealit json")
+
+    st.json({
+        'foo': 'bar',
+        'baz': 'boz',
+        'stuff': [
+            'stuff 1',
+            'stuff 2',
+            'stuff 3',
+            'stuff 5',
+        ],
+    })
+
+with tab4:
+    # 4.  Chart Element
+    st.header("4. Chart Element")
+    st.header("4a. Strealit Line Chart")
+    st.line_chart(df)
+
+    st.header("4b. Strealit Area Chart")
+    st.area_chart(df)
+
+    st.write("4c. Strealit Bar Chart
+    ''')
+    st.bar_chart(df)
+
+    st.write('''
+    # 4d. Strealit Pyplot
     ''')
 
+    arr = np.random.normal(1, 1, size=100)
+    fig, ax = plt.subplots()
+    ax.hist(arr, bins=20)
 
-# 3.  Data Display Element
-st.write('''
-# 3a. Streamlit Dataframe
-''')
-df = pd.DataFrame(
-   np.random.randn(50, 3),
-   columns=('col %d' % i for i in range(3)))
+    st.pyplot(fig)
 
-st.dataframe(df)
-st.write('''
-# 3b. Streamlit Table
-''')
-st.table(df)
+    st.write('''
+    # 4e. Streamlit Altair
+    ''')
 
-st.write('''
-# 3c. Strealit Metric
-''')
+    chart_data = pd.DataFrame(
+        np.random.randn(20, 3),
+        columns=['a', 'b', 'c'])
 
-col1, col2, col3 = st.columns(3)
-col1.metric("Temperature", "70 °F", "1.2 °F")
-col2.metric("Wind", "9 mph", "-8%")
-col3.metric("Humidity", "86%", "4%")
-col3.metric(label="Humidity",value="86%",delta="4%",delta_color="inverse")
+    c = alt.Chart(chart_data).mark_circle().encode(
+        x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
 
-st.write('''
-# 3d. Strealit json
-''')
+    st.altair_chart(c, use_container_width=True)
 
-st.json({
-    'foo': 'bar',
-    'baz': 'boz',
-    'stuff': [
-        'stuff 1',
-        'stuff 2',
-        'stuff 3',
-        'stuff 5',
-    ],
-})
+    st.write('''
+    # 4f. Streamlit Vega Lite Chart
+    ''')
 
-# 4.  Chart Element
+    st.vega_lite_chart(chart_data, {
+        'mark': {'type': 'circle', 'tooltip': True},
+        'encoding': {
+            'x': {'field': 'a', 'type': 'quantitative'},
+            'y': {'field': 'b', 'type': 'quantitative'},
+            'size': {'field': 'c', 'type': 'quantitative'},
+            'color': {'field': 'c', 'type': 'quantitative'},
+        },
+    })
 
-st.write('''
-# 4a. Strealit Line Chart
-''')
-st.line_chart(df)
+    st.write('''
+    # 4f. Streamlit Plotly Chart
+    # ''')
 
-st.write('''
-# 4b. Strealit Area Chart
-''')
-st.area_chart(df)
+    # x1 = np.random.randn(200) - 2
+    # x2 = np.random.randn(200)
+    # x3 = np.random.randn(200) + 2
 
-st.write('''
-# 4c. Strealit Bar Chart
-''')
-st.bar_chart(df)
+    # # Group data together
+    # hist_data = [x1, x2, x3]
 
-st.write('''
-# 4d. Strealit Pyplot
-''')
+    # group_labels = ['Group 1', 'Group 2', 'Group 3']
 
-arr = np.random.normal(1, 1, size=100)
-fig, ax = plt.subplots()
-ax.hist(arr, bins=20)
+    # # Create distplot with custom bin_size
+    # fig = ff.create_distplot(
+    #         hist_data, group_labels, bin_size=[.1, .25, .5])
 
-st.pyplot(fig)
-
-st.write('''
-# 4e. Streamlit Altair
-''')
-
-chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=['a', 'b', 'c'])
-
-c = alt.Chart(chart_data).mark_circle().encode(
-    x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
-
-st.altair_chart(c, use_container_width=True)
-
-st.write('''
-# 4f. Streamlit Vega Lite Chart
-''')
-
-st.vega_lite_chart(chart_data, {
-    'mark': {'type': 'circle', 'tooltip': True},
-    'encoding': {
-        'x': {'field': 'a', 'type': 'quantitative'},
-        'y': {'field': 'b', 'type': 'quantitative'},
-        'size': {'field': 'c', 'type': 'quantitative'},
-        'color': {'field': 'c', 'type': 'quantitative'},
-    },
-})
-
-st.write('''
-# 4f. Streamlit Plotly Chart
-# ''')
-
-# x1 = np.random.randn(200) - 2
-# x2 = np.random.randn(200)
-# x3 = np.random.randn(200) + 2
-
-# # Group data together
-# hist_data = [x1, x2, x3]
-
-# group_labels = ['Group 1', 'Group 2', 'Group 3']
-
-# # Create distplot with custom bin_size
-# fig = ff.create_distplot(
-#         hist_data, group_labels, bin_size=[.1, .25, .5])
-
-# # Plot!
-# st.plotly_chart(fig, use_container_width=True)
+    # # Plot!
+    # st.plotly_chart(fig, use_container_width=True)
 
 
-st.write('''
-# 4g. Streamlit Bokeh Chart
-''')
-x = [1, 2, 3, 4, 5]
-y = [6, 7, 2, 4, 5]
+    st.write('''
+    # 4g. Streamlit Bokeh Chart
+    ''')
+    x = [1, 2, 3, 4, 5]
+    y = [6, 7, 2, 4, 5]
 
-p = figure(
-    title='simple line example',
-    x_axis_label='x',
-    y_axis_label='y')
+    p = figure(
+        title='simple line example',
+        x_axis_label='x',
+        y_axis_label='y')
 
-p.line(x, y, legend_label='Trend', line_width=2)
+    p.line(x, y, legend_label='Trend', line_width=2)
 
-st.bokeh_chart(p, use_container_width=True)
+    st.bokeh_chart(p, use_container_width=True)
 
-st.write('''
-# 4h. Streamlit Pydeck Chart
-''')
+    st.write('''
+    # 4h. Streamlit Pydeck Chart
+    ''')
 
-chart_data = pd.DataFrame(
-   np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-   columns=['lat', 'lon'])
-
-st.pydeck_chart(pdk.Deck(
-    map_style=None,
-    initial_view_state=pdk.ViewState(
-        latitude=37.76,
-        longitude=-122.4,
-        zoom=11,
-        pitch=50,
-    ),
-    layers=[
-        pdk.Layer(
-           'HexagonLayer',
-           data=chart_data,
-           get_position='[lon, lat]',
-           radius=200,
-           elevation_scale=4,
-           elevation_range=[0, 1000],
-           pickable=True,
-           extruded=True,
-        ),
-        pdk.Layer(
-            'ScatterplotLayer',
-            data=chart_data,
-            get_position='[lon, lat]',
-            get_color='[200, 30, 0, 160]',
-            get_radius=200,
-        ),
-    ],
-))
-st.write('''
-# 4i. Streamlit Graphviz Chart
-''')
-
-graph = graphviz.Digraph()
-graph.edge('run', 'intr')
-graph.edge('intr', 'runbl')
-graph.edge('runbl', 'run')
-graph.edge('run', 'kernel')
-graph.edge('kernel', 'zombie')
-graph.edge('kernel', 'sleep')
-graph.edge('kernel', 'runmem')
-graph.edge('sleep', 'swap')
-graph.edge('swap', 'runswap')
-graph.edge('runswap', 'new')
-graph.edge('runswap', 'runmem')
-graph.edge('new', 'runmem')
-graph.edge('sleep', 'runmem')
-
-st.graphviz_chart(graph)
-
-st.write('''
-# 4j. Streamlit Map Chart
-''')
-
-df = pd.DataFrame(
+    chart_data = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
     columns=['lat', 'lon'])
 
-st.map(df)
-# 5.  Input Widget
-st.header("5 Input Widget")
-st.write('''
-# 5a. Streamlit Button
-''')
-
-if st.button('Say hello'):
-    st.write('Why hello there')
-else:
-    st.write('Goodbye')
-
-st.header("5b. Streamlit Experimental_data_editor")
-
-df = pd.DataFrame(
-    [
-       {"command": "st.selectbox", "rating": 4, "is_widget": True},
-       {"command": "st.balloons", "rating": 3, "is_widget": True},
-       {"command": "st.time_input", "rating": 3, "is_widget": True},
-   ]
-)
-
-edited_df = st.experimental_data_editor(df)
-
-
-favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
-st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
-
-
-st.header("5c. Streamlit Button Download ")
-csv = pd.read_csv(st.secrets['public_gsheet_csv']).to_csv()
-
-st.download_button(label="Download File Data",data=csv,file_name="data.csv",mime='text/csv')
-
-st.header("5d. Streamlit CheckBox ")
-agree = st.checkbox('I agree')
-
-if agree:
-    st.write('Great!')
-
-st.header("5e. Streamlit Radio ")
-genre = st.radio(
-    "What's your favorite movie genre",
-    ('Comedy', 'Drama', 'Documentary'))
-
-if genre == 'Comedy':
-    st.write('You selected comedy.')
-else:
-    st.write("You didn\'t select comedy.")
-
-st.header("5e. Streamlit Select Box ")
-option = st.selectbox(
-    'How would you like to be contacted?',
-    ('Email', 'Home phone', 'Mobile phone'))
-
-st.write('You selected:', option)
-
-st.header("5f. Streamlit Multi Select ")
-options = st.multiselect(
-    'What are your favorite colors',
-    ['Green', 'Yellow', 'Red', 'Blue'],
-    ['Yellow', 'Red'])
-
-st.write('You selected:', options)
-
-st.header("5g. Streamlit Slider ")
-values = st.slider(
-    'Select a range of values',
-    0.0, 100.0, (25.0, 75.0))
-st.write('Values:', values)
-
-st.header("5g. Streamlit Select Slider ")
-color = st.select_slider(
-    'Select a color of the rainbow',
-    options=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
-st.write('My favorite color is', color)
-
-st.header("5h. Streamlit Text Input ")
-text = st.text_input('Title Write', 'Text')
-st.write('Your Text is', text)
-
-st.header("5i. Streamlit Number Input ")
-number = st.number_input('Insert a number')
-st.write('The current number is ', number)
-
-st.header("5j. Streamlit Text Area ")
-txt = st.text_area('Text to analyze', '''
-    It was the best of times, it was the worst of times, it was
-    the age of wisdom, it was the age of foolishness, it was
-    the epoch of belief, it was the epoch of incredulity, it
-    was the season of Light, it was the season of Darkness, it
-    was the spring of hope, it was the winter of despair, (...)
+    st.pydeck_chart(pdk.Deck(
+        map_style=None,
+        initial_view_state=pdk.ViewState(
+            latitude=37.76,
+            longitude=-122.4,
+            zoom=11,
+            pitch=50,
+        ),
+        layers=[
+            pdk.Layer(
+            'HexagonLayer',
+            data=chart_data,
+            get_position='[lon, lat]',
+            radius=200,
+            elevation_scale=4,
+            elevation_range=[0, 1000],
+            pickable=True,
+            extruded=True,
+            ),
+            pdk.Layer(
+                'ScatterplotLayer',
+                data=chart_data,
+                get_position='[lon, lat]',
+                get_color='[200, 30, 0, 160]',
+                get_radius=200,
+            ),
+        ],
+    ))
+    st.write('''
+    # 4i. Streamlit Graphviz Chart
     ''')
-st.write(txt)
 
-st.header("5k. Streamlit Date Input ")
-d = st.date_input(
-    "When\'s your birthday",
-    datetime.date(2023, 3, 19))
-st.write('Your birthday is:', d)
+    graph = graphviz.Digraph()
+    graph.edge('run', 'intr')
+    graph.edge('intr', 'runbl')
+    graph.edge('runbl', 'run')
+    graph.edge('run', 'kernel')
+    graph.edge('kernel', 'zombie')
+    graph.edge('kernel', 'sleep')
+    graph.edge('kernel', 'runmem')
+    graph.edge('sleep', 'swap')
+    graph.edge('swap', 'runswap')
+    graph.edge('runswap', 'new')
+    graph.edge('runswap', 'runmem')
+    graph.edge('new', 'runmem')
+    graph.edge('sleep', 'runmem')
 
-st.header("5l. Streamlit Time Input ")
-t = st.time_input('Set an alarm for', datetime.time(8, 45))
-st.write('Alarm is set for', t)
+    st.graphviz_chart(graph)
+
+    st.write('''
+    # 4j. Streamlit Map Chart
+    ''')
+
+    df = pd.DataFrame(
+        np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+        columns=['lat', 'lon'])
+
+    st.map(df)
+
+with tab5:
+    # 5.  Input Widget
+    st.header("5 Input Widget")
+    st.write('''
+    # 5a. Streamlit Button
+    ''')
+
+    if st.button('Say hello'):
+        st.write('Why hello there')
+    else:
+        st.write('Goodbye')
+
+    st.header("5b. Streamlit Experimental_data_editor")
+
+    df = pd.DataFrame(
+        [
+        {"command": "st.selectbox", "rating": 4, "is_widget": True},
+        {"command": "st.balloons", "rating": 3, "is_widget": True},
+        {"command": "st.time_input", "rating": 3, "is_widget": True},
+    ]
+    )
+
+    edited_df = st.experimental_data_editor(df)
 
 
-st.header("5m. Streamlit File Uploader ")
-uploaded_file = st.file_uploader("Choose a file CSV")
-if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.table(dataframe)
+    favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
+    st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
 
 
-picture = st.camera_input("Take a picture")
+    st.header("5c. Streamlit Button Download ")
+    csv = pd.read_csv(st.secrets['public_gsheet_csv']).to_csv()
 
-if picture:
-    st.image(picture)
+    st.download_button(label="Download File Data",data=csv,file_name="data.csv",mime='text/csv')
+
+    st.header("5d. Streamlit CheckBox ")
+    agree = st.checkbox('I agree')
+
+    if agree:
+        st.write('Great!')
+
+    st.header("5e. Streamlit Radio ")
+    genre = st.radio(
+        "What's your favorite movie genre",
+        ('Comedy', 'Drama', 'Documentary'))
+
+    if genre == 'Comedy':
+        st.write('You selected comedy.')
+    else:
+        st.write("You didn\'t select comedy.")
+
+    st.header("5e. Streamlit Select Box ")
+    option = st.selectbox(
+        'How would you like to be contacted?',
+        ('Email', 'Home phone', 'Mobile phone'))
+
+    st.write('You selected:', option)
+
+    st.header("5f. Streamlit Multi Select ")
+    options = st.multiselect(
+        'What are your favorite colors',
+        ['Green', 'Yellow', 'Red', 'Blue'],
+        ['Yellow', 'Red'])
+
+    st.write('You selected:', options)
+
+    st.header("5g. Streamlit Slider ")
+    values = st.slider(
+        'Select a range of values',
+        0.0, 100.0, (25.0, 75.0))
+    st.write('Values:', values)
+
+    st.header("5g. Streamlit Select Slider ")
+    color = st.select_slider(
+        'Select a color of the rainbow',
+        options=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+    st.write('My favorite color is', color)
+
+    st.header("5h. Streamlit Text Input ")
+    text = st.text_input('Title Write', 'Text')
+    st.write('Your Text is', text)
+
+    st.header("5i. Streamlit Number Input ")
+    number = st.number_input('Insert a number')
+    st.write('The current number is ', number)
+
+    st.header("5j. Streamlit Text Area ")
+    txt = st.text_area('Text to analyze', '''
+        It was the best of times, it was the worst of times, it was
+        the age of wisdom, it was the age of foolishness, it was
+        the epoch of belief, it was the epoch of incredulity, it
+        was the season of Light, it was the season of Darkness, it
+        was the spring of hope, it was the winter of despair, (...)
+        ''')
+    st.write(txt)
+
+    st.header("5k. Streamlit Date Input ")
+    d = st.date_input(
+        "When\'s your birthday",
+        datetime.date(2023, 3, 19))
+    st.write('Your birthday is:', d)
+
+    st.header("5l. Streamlit Time Input ")
+    t = st.time_input('Set an alarm for', datetime.time(8, 45))
+    st.write('Alarm is set for', t)
 
 
-st.header("5n. Streamlit Color Picker ")
-color = st.color_picker('Pick A Color', '#00f900')
-st.write('The current color is', color)
-# 6.  Media Element
-st.header("6. Media Element ")
-dir = os.getcwd()
-st.write(dir)
-image = Image.open('./Thumnail Paladin #2.png')
+    st.header("5m. Streamlit File Uploader ")
+    uploaded_file = st.file_uploader("Choose a file CSV")
+    if uploaded_file is not None:
+        # To read file as bytes:
+        bytes_data = uploaded_file.getvalue()
+        st.write(bytes_data)
+        # Can be used wherever a "file-like" object is accepted:
+        dataframe = pd.read_csv(uploaded_file)
+        st.table(dataframe)
 
-st.image(image, caption='Channel Youtube : EVAN141')
+
+    picture = st.camera_input("Take a picture")
+
+    if picture:
+        st.image(picture)
+
+
+    st.header("5n. Streamlit Color Picker ")
+    color = st.color_picker('Pick A Color', '#00f900')
+    st.write('The current color is', color)
+with tab6:
+    # 6.  Media Element
+    st.header("6. Media Element ")
+    dir = os.getcwd()
+    st.write(dir)
+    image = Image.open('./Thumnail Paladin #2.png')
+
+    st.image(image, caption='Channel Youtube : EVAN141')
 
 
 # 7.  Layout and Container
+with tab7:
+    st.write("👆 This is Tab")
+
 # 8.  Status Element
 # 9.  Control Flow
 # 10. Utilities
