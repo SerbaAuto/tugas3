cf8675a5712670851534a904b80bb88d05c37989
SerbaAuto <87936278+SerbaAuto@users.noreply.github.com>
2023-03-19 12:43:06 +0700

    -

--- Main.py
+++ Main.py
@@ -243,7 +243,6 @@ df = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
     columns=['lat', 'lon'])
 
-st.table(df)
 st.map(df)
 # 5.  Input Widget
 st.header("5 Input Widget")
