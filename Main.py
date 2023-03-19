a6fdeb0f39413571f7f4b3fb19916626e56dad21
SerbaAuto <87936278+SerbaAuto@users.noreply.github.com>
2023-03-19 13:03:22 +0700

    -

--- Main.py
+++ Main.py
@@ -260,7 +260,7 @@ st.header("5b. Streamlit Experimental_data_editor")
 df = pd.DataFrame(
     [
        {"command": "st.selectbox", "rating": 4, "is_widget": True},
-       {"command": "st.balloons", "rating": 3, "is_widget": False},
+       {"command": "st.balloons", "rating": 3, "is_widget": True},
        {"command": "st.time_input", "rating": 3, "is_widget": True},
    ]
 )
@@ -268,7 +268,7 @@ df = pd.DataFrame(
 edited_df = st.experimental_data_editor(df)
 
 
-favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
+favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["rating"]
 st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
 # 6.  Media Element
 # 7.  Layout and Container
